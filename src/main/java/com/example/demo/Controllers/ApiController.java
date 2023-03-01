96607df618fb392e47fb0780b17ce538698612c9
Ananyaavr <727721euai003@skcet.ac.in>
2023-03-01 20:54:47 +0530

    Day4 portal

--- src/main/java/com/example/demo/Controllers/ApiController.java
+++ src/main/java/com/example/demo/Controllers/ApiController.java
@@ -1,10 +1,15 @@
-package com.example.demo.Controllers;
+ 	package com.example.demo.Controllers;
+
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.Models.Student;
@@ -17,12 +22,24 @@ public class ApiController {
 StudentRepo serviceRepository;
 @Autowired
 ApiService service;
-@PutMapping("/student")
+@GetMapping("/")
+List<Student> getlist(){
+	return serviceRepository.findAll();
+}
+@GetMapping("/{id}")
+public Optional<Student> getbyid(int id){
+	return service.getStudent(id);
+}
+@PutMapping("/{id}")
 public String update(@RequestBody Student stu) {
 	return service.updateDetails(stu);
 }
-@DeleteMapping("/student")
-public String delete(@RequestParam int id) {
+@DeleteMapping("/{id}")
+public String delete(@PathVariable int id) {
 	return service.deleteDetails(id);
+}
+@PostMapping("/")
+public Student create(@RequestBody Student stu) {
+return serviceRepository.save(stu);
 }
 }
